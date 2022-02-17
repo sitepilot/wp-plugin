bf350471a62782a5af06bbcf3262063fc3db8560
Nick Jansen <n.jansen@sitepilot.io>
2022-02-17 21:33:37 +0000

    Refactor type hints and docblocks

--- src/Support/PluginProvider.php
+++ src/Support/PluginProvider.php
@@ -6,18 +6,8 @@ use Sitepilot\WpFramework\Support\Application;
 
 abstract class PluginProvider extends Application
 {
-    /**
-     * The WordPress plugin data.
-     *
-     * @var array
-     */
     protected array $plugin;
 
-    /**
-     * Register plugin services.
-     *
-     * @return void
-     */
     public function __register(): void
     {
         $this->plugin = get_file_data($this->file, [
@@ -25,22 +15,11 @@ abstract class PluginProvider extends Application
         ], 'plugin');
     }
 
-    /**
-     * Returns the plugin version.
-     *
-     * @return string
-     */
     public function get_version(): string
     {
         return $this->plugin['version'] ?? '';
     }
 
-    /**
-     * Get plugin url path.
-     *
-     * @param string $path
-     * @return string
-     */
     public function get_url(string $path = ''): string
     {
         return plugins_url($path, $this->file);
